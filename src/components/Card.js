4e87188418a1b556a9ae6f7925f5cc21834571ea
Jacob Chen <jacobpchen@gmail.com>
2021-01-26 15:59:44 -0500

    implementing a views counter

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,16 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Card = ({ recipe }) => {
+    console.log(recipe)
+
+    const imageClick = () => {
+        console.log(recipe.title)
+        console.log('Image clicked!')
+    }
 
     return (
         <Link to={`/recipes/${recipe.id}`} className="uk-link-reset">
-
 
             <div className="uk-card uk-card-muted">
                 <div className="uk-card-media-top">
                     <img
                         src={recipe.link}
                         height="100"
+                        onClick={imageClick}
                     />
                 </div>
                 <div className="uk-card-body">
--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -5,8 +5,6 @@ export const UserContext = createContext(null)
 export default ({ children }) => {
 
     const [user, setUser] = useState(null)
-    console.log("INSIDE CONTEXT - USERCONTEXT")
-    console.log(user)
 
     return (
         <UserContext.Provider value={{ user, setUser }}>
